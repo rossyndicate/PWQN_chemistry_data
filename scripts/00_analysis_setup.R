99a1ab4d90a77e9cfafefb56d19c4451f09bed14
SamStruthers <sam.struthers2000@gmail.com>
2024-01-02 09:19:56 -0700

    Set up scripts complete

---
+++ scripts/00_analysis_setup.R
@@ -0,0 +1,36 @@
+#this file contains all the packages,metadata, groupings and color palettes that are used in downstream scripts
+
+### ----- Load packages ----- ###
+package_load <- function(package_names){
+  for(i in 1:length(package_names)){
+    if(!package_names[i] %in% installed.packages()){
+      install.packages(package_names[i])
+    }
+    library(package_names[i],character.only = TRUE)
+  }
+}
+
+#vector of packages
+pack_req <- c( 
+  # data wrangling packages
+  "tidyverse","lubridate","padr","janitor","padr", "broom","arrow","readxl",
+  #spatial packages
+  "sf","terra","nhdplusTools", "tigris","raster", "leaflet","tmap",
+  # plotting
+  "ggpubr","ggthemes","scales","corrplot","gghighlight", "geomtextpath", "ggbeeswarm","plotly", "ggpmisc","flextable",
+  # web scrapping
+  "rjson", "rvest", "dataRetrieval", "httr", "jsonlite",
+  #extra
+  "devtools", "trend")
+package_load(pack_req)
+
+
+remove(pack_req, package_load)
+#Simple function to negate %in%
+`%nin%` = Negate(`%in%`)
+
+
+#------ Metadata Files ------#
+
+# site_meta: Locations of each site, includes which project it is on, common name and sonde housing notes
+site_meta <- read.csv('data/metadata/sonde_location_metadata.csv')
